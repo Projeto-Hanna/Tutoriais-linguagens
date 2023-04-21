d0ed85499b3408a46d53c322ec88a39676832e15
Projeto Hanna <124400235+Projeto-Hanna@users.noreply.github.com>
2023-04-21 11:11:34 -0300

    Add files via upload

---
+++ Java/soma.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String args[]) {
+        int x = 2;
+        int y = 3;
+        int soma = x + y;
+        System.out.println("Minha soma deu: " + soma);
+    }
+}
